159e0f64ea309db96de2c245e7021c2cd78d4bbc
nikoletabeyska <76749430+nikoletabeyska@users.noreply.github.com>
2024-02-06 05:07:00 +0200

    Added client routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,9 @@
 import _ from 'lodash';
+import { Router } from '@vaadin/router';
+import { Access } from './components/access'
+import { Login } from './components/login'
+import { Register } from './components/register'
+import { Home } from './components/home'
 
 function component() {
     const element = document.createElement('div');
@@ -9,3 +14,14 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+const routes = [
+    {path: '/', component: Access.selector},
+    {path: '/auth/login', component: Login.selector},
+    {path: '/auth/register', component: Register.selector},
+    {path: '/home', component: Home.selector},
+
+]
+
+const router = new Router(document.querySelector('#outlet'));
+router.setRoutes(routes);
