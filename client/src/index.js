25101719fa8c5c3e3072c719b6045d976e203cd2
nikoletabeyska <76749430+nikoletabeyska@users.noreply.github.com>
2024-02-10 13:18:54 +0200

    Creating client-side routing

--- client/src/index.js
+++ client/src/index.js
@@ -4,22 +4,15 @@ import { Access } from './components/access'
 import { Login } from './components/login'
 import { Register } from './components/register'
 import { Home } from './components/home'
-
-function component() {
-    const element = document.createElement('div');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
+//import { Game } from './components/game'
+import { Game } from './components/game'
 
 const routes = [
     {path: '/', component: Access.selector},
     {path: '/auth/login', component: Login.selector},
     {path: '/auth/register', component: Register.selector},
     {path: '/home', component: Home.selector},
+    {path: '/game', component: Game.selector},
 
 ]
 
