ad33bf43a495505109b25aca02bce1f1a39b51b3
nikoletabeyska <76749430+nikoletabeyska@users.noreply.github.com>
2024-02-10 13:18:29 +0200

    Configuration

---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    serverBaseUrl: "http://127.0.0.1:3000", 
+  };
+  
+  export { config };
+  
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,6 +13,7 @@ const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
     devServer: {
         open: true,
