01e0a3834ca182738dbc05406dc19875e3b36f29
nikoletabeyska <76749430+nikoletabeyska@users.noreply.github.com>
2024-02-10 13:22:06 +0200

    Adding validation schemas on the client

---
+++ client/src/services/validationSchemas.js
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+export const RegistrationInputSchema = z.object({
+    name: z
+        .string()
+        .min(2, "Name should have at least 2 alphabets")
+        .refine(
+            value => /^[a-zA-Z]+[-'s]?[a-zA-Z ]+$/.test(value),
+            "Name should contain only alphabets!"
+        )
+        .refine(
+            value => /^[a-zA-Z]+\s+[a-zA-Z]+$/.test(value),
+            "Please enter both first name and last name!"
+        ),
+
+    email: z
+        .string()
+        .min(5)
+        .email("Email must be valid!"),
+    password: z.string().min(8, { message: "Password must be at least 8 symbols!"})
+
+})
+
+
+export const LoginInputSchema = z.object({
+    email: z
+    .string()
+    .email("Email must be valid!"),
+
+    password: z.string().min(8, { message: "Password must be at least 8 symbols!"})
+
+})
+
