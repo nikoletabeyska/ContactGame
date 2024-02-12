4856b513f7bb7e5f9a7090767ae814f101c94cce
ellisaveta <elina123456@mail.bg>
2024-02-13 01:25:43 +0200

    Secure create game end point on server

--- server/src/routers/game.js
+++ server/src/routers/game.js
@@ -4,15 +4,17 @@ import {
     requestHandler,
     ResponseWithCode
 } from "../middlewares/request-handler.js"
+import { authMiddleware } from '../middlewares/auth.js';
 
 const gameRouter = Router()
 const gameService = new GameService()
 
 gameRouter.post(
     "/create",
+    authMiddleware,
     requestHandler(async req => {
         const game = await gameService.create(input)
-        // return new ResponseWithCode(201, { name: user.name })
+        return new ResponseWithCode(201, { game: game })
     })
 )
 
